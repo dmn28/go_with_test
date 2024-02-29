552129befffaacf36f839022ae28f579a371ed46
dmn28 <damjantzar@gmail.com>
2024-02-29 16:49:06 +0100

    fix hello_test.go

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -7,16 +7,13 @@ func TestHello(t *testing.T) {
 		got := hello("Chris")
 		want := "Hello, Chris"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assert_correct_message(t, got, want)
 	})
 	t.Run("say 'Hello World' when empty string passed", func(t *testing.T) {
 		got := hello("")
 		want := "Hello, World!"
 
-		if got != want {
-		}
+		assert_correct_message(t, got, want)
 	})
 }
 
